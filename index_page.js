e785183da627c29006792c2623864edbc5b2ed4f
Gabriel Garcia <gpgarcia14@hotmail.com>
2025-07-02 11:41:11 -0300

    Add files via upload

---
+++ index_page.js
@@ -0,0 +1,22 @@
+
+
+function show_image() {
+  const img = document.createElement("img");
+  img.src = "lancer-spin.gif";
+  img.className = "boy";
+
+    const maxWidth = window.innerWidth - 100; // Considera largura da imagem
+    const maxHeight = window.innerHeight - 100;
+
+    const posX = Math.random() * maxWidth;
+    const posY = Math.random() * maxHeight;
+    
+        img.style.position = 'absolute';
+        img.style.left = posX + "px";
+        img.style.top = posY + "px";
+
+  document.body.appendChild(img);
+}
+
+
+
